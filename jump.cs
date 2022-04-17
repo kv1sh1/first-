a53ec7955b8e48b38b115f6c058be63ec136c4b9
kv1sh1 <94894703+kv1sh1@users.noreply.github.com>
2022-04-17 16:18:59 +0500

    Add files via upload

---
+++ jump.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class jump : MonoBehaviour {
+
+
+	void Update () {
+		if (Input.GetKey(KeyCode.Space))
+        {
+            transform.Translate(new Vector2(0, 0.5f));
+        }
+	}
+}
